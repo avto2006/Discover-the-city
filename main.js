17caca3ae467a0f1fa5ede2d7a0b8ad77ec266fa
Avto Bagaturia <Avtobagaturia21@gmail.com>
2025-08-10 20:33:21 +0400

    some changes

--- main.js
+++ main.js
@@ -2,3 +2,14 @@ const inp1 = document.getElementById('inp1');
 const inp2 = document.getElementById('inp2');
 const btn = document.getElementById('btn1');
 const root = document.getElementById('root');
+
+document.getElementById('btn').addEventListener('click', function() {
+     const inp1 = document.getElementById('inp1').value.trim();
+     const inp2 = document.getElementById('inp2').value.trim();
+
+     if(inp1 && inp2) {
+
+     } else {
+        document.getElementById('root').textContent = "გთოვთ შეიყვანოთ ორივე ქალაქი"
+     }
+})
