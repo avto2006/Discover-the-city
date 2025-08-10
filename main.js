73ae30d1c6f404c47fb91a24008545ad8d2c1960
Avto Bagaturia <Avtobagaturia21@gmail.com>
2025-08-10 19:21:42 +0400

    some changes

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
 const inp1 = document.getElementById('inp1');
 const inp2 = document.getElementById('inp2');
+const btn = document.getElementById('btn1');
+const root = document.getElementById('root');
